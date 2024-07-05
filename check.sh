9b153089751b94d5013856625c875567df26c7bd
abdur-rahman54 <abdur.rahman59354@gmail.com>
2024-07-05 20:57:10 +0600

    Automate Minecraft server setup

--- check.sh
+++ check.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Use the environment variables passed by GitHub Actions
-IMAGE_ID=$IMAGE_ID
+AMI_ID=$IMAGE_ID
 INSTANCE_TYPE=$INSTANCE_TYPE
-KEY_PAIR_NAME=$KEY_PAIR_NAME
+KEY_PAIR_NAME=$KEY_PAIR
 SECURITY_GROUP_NAME=$SECURITY_GROUP_NAME
 
 # Debugging: Print the environment variables
-echo "Using Image ID: $IMAGE_ID"
+echo "Using Image ID: $AMI_ID"
 echo "Using Instance Type: $INSTANCE_TYPE"
 echo "Using Key Pair Name: $KEY_PAIR_NAME"
 echo "Using Security Group Name: $SECURITY_GROUP_NAME"
